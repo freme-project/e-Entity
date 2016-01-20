54a876b5f84b7e572d66bb8ac42ff4045e11c5a3
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2016-01-20 15:08:07 +0100

    linking of single entity feat. https://github.com/freme-project/e-Entity/issues/67

--- src/main/java/eu/freme/eservices/eentity/api/EEntityService.java
+++ src/main/java/eu/freme/eservices/eentity/api/EEntityService.java
@@ -17,6 +17,10 @@
  */
 package eu.freme.eservices.eentity.api;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 
@@ -26,6 +30,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 import eu.freme.eservices.eentity.exceptions.BadRequestException;
 import eu.freme.eservices.eentity.exceptions.ExternalServiceFailedException;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -94,29 +99,72 @@ public class EEntityService {
                     modes += ","+m;
                 }                
             }
-//            System.out.println(text);
-//            System.out.println(URLDecoder.decode(text, "UTF-8"));
-            System.out.println("links: "+numLinks);
-            System.out.println("requesting: "+fremeNERURL);
-            HttpResponse<String> response = Unirest.post(fremeNERURL+"?language="+languageParam+"&dataset="+dataset+"&prefix="+URLEncoder.encode(prefix,"UTF-8")+"&numLinks="+numLinks+"&mode="+modes)
-                    .header("Content-Type", informat+"; charset=UTF-8")
-//                    .header("Content-Type", "text/plain; charset=UTF-8")
-//                    .body(URLEncoder.encode(text, "UTF-8").replaceAll("\\+", " ")).asString();
-                    .body(text).asString();
             
-//            HttpResponse<String> response = Unirest.post(fremeNERURL+"language="+languageParam+"&dataset="+dataset)
-//                    .header("Content-Type", "application/x-www-form-urlencoded")
-//                    .body("i="+URLEncoder.encode(text, "UTF-8")).asString();
+//            for(String m : rMode) {
+//                System.out.println("mode: " + m);
+//            }
+//            
+//            System.out.println("text: " + text);
+//            System.out.println("informat: " + informat);
             
-            if (response.getStatus() != HttpStatus.OK.value()) {
-                if( response.getStatus() == HttpStatus.BAD_REQUEST.value() ) {
-                    throw new BadRequestException(response.getBody());
-                } else {
-                    throw new ExternalServiceFailedException(response.getBody());
+            if(informat.equals("text/plain") && rMode.size() == 1 && modes.equals("link") ) {
+                
+                Model m = ModelFactory.createDefaultModel();
+                Resource strRes = m.createResource(prefix+"#char=0,"+text.length());
+                strRes.addProperty(RDF.type, m.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#Context"));
+                strRes.addProperty(RDF.type, m.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#RFC5147String"));
+                strRes.addProperty(RDF.type, m.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#String"));
+                strRes.addProperty(RDF.type, m.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#Phrase"));
+                strRes.addLiteral(m.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf"), text);
+                strRes.addLiteral(m.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#beginIndex"), 0);
+                strRes.addLiteral(m.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#endIndex"), text.length());
+                strRes.addProperty(m.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#referenceContext"), strRes);
+                strRes.addLiteral(m.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString"), text);
+                
+                String syntax = "TTL";
+                StringWriter out = new StringWriter();
+                m.write(out, syntax);
+                String result = out.toString();
+
+//                System.out.println("data:"+ result);
+//                System.out.println("path: " + fremeNERURL+"?language="+languageParam+"&dataset="+dataset+"&prefix="+URLEncoder.encode(prefix,"UTF-8")+"&numLinks="+numLinks+"&mode="+modes);
+                
+                HttpResponse<String> response = Unirest.post(fremeNERURL+"?language="+languageParam+"&dataset="+dataset+"&prefix="+URLEncoder.encode(prefix,"UTF-8")+"&numLinks="+numLinks+"&mode="+modes)
+                        .header("Content-Type", "text/turtle; charset=UTF-8")
+                        .body(result).asString();
+                
+                if (response.getStatus() != HttpStatus.OK.value()) {
+                    if( response.getStatus() == HttpStatus.BAD_REQUEST.value() ) {
+                        throw new BadRequestException(response.getBody());
+                    } else {
+                        throw new ExternalServiceFailedException(response.getBody());
+                    }
                 }
+                String nif = response.getBody();
+                return nif;
+                
+            } else {
+            
+                HttpResponse<String> response = Unirest.post(fremeNERURL+"?language="+languageParam+"&dataset="+dataset+"&prefix="+URLEncoder.encode(prefix,"UTF-8")+"&numLinks="+numLinks+"&mode="+modes)
+                        .header("Content-Type", informat+"; charset=UTF-8")
+    //                    .header("Content-Type", "text/plain; charset=UTF-8")
+    //                    .body(URLEncoder.encode(text, "UTF-8").replaceAll("\\+", " ")).asString();
+                        .body(text).asString();
+
+    //            HttpResponse<String> response = Unirest.post(fremeNERURL+"language="+languageParam+"&dataset="+dataset)
+    //                    .header("Content-Type", "application/x-www-form-urlencoded")
+    //                    .body("i="+URLEncoder.encode(text, "UTF-8")).asString();
+
+                if (response.getStatus() != HttpStatus.OK.value()) {
+                    if( response.getStatus() == HttpStatus.BAD_REQUEST.value() ) {
+                        throw new BadRequestException(response.getBody());
+                    } else {
+                        throw new ExternalServiceFailedException(response.getBody());
+                    }
+                }
+                String nif = response.getBody();
+                return nif;
             }
-            String nif = response.getBody();
-            return nif;
         } catch (UnirestException e) {
             throw new ExternalServiceFailedException(e.getMessage());
         } catch (UnsupportedEncodingException ex) {
